a3c05ef504907d66617fb4f4b49947415a125abb
MrityunjayNPandey <pandeymrityunjay796@gmail.com>
2023-12-13 04:37:26 +0530

    Add crop yield prediction web application with installation and usage instructions

--- backend/yield_prediction.py
+++ backend/yield_prediction.py
@@ -1,29 +1,31 @@
-import pandas as pd
 import numpy as np
-import json
-import sys
+import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+
+
 def predict(state, district, season, crop, area):
     # Load the dataset
     df = pd.read_csv("ML/yield_prediction/crop_production_karnataka.csv")
 
     # Drop the Crop_Year column
-    df = df.drop(['Crop_Year'], axis=1)
+    df = df.drop(["Crop_Year"], axis=1)
 
     # Separate the features and target variables
-    X = df.drop(['Production'], axis=1)
-    y = df['Production']
+    X = df.drop(["Production"], axis=1)
+    y = df["Production"]
 
     # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
     # Categorical columns for one-hot encoding
-    categorical_cols = ['State_Name', 'District_Name', 'Season', 'Crop']
+    categorical_cols = ["State_Name", "District_Name", "Season", "Crop"]
 
     # One-hot encode the categorical columns
-    ohe = OneHotEncoder(handle_unknown='ignore')
+    ohe = OneHotEncoder(handle_unknown="ignore")
     ohe.fit(X_train[categorical_cols])
 
     # Convert categorical columns to one-hot encoding
@@ -31,8 +33,12 @@ def predict(state, district, season, crop, area):
     X_test_categorical = ohe.transform(X_test[categorical_cols])
 
     # Combine the one-hot encoded categorical columns and numerical columns
-    X_train_final = np.hstack((X_train_categorical.toarray(), X_train.drop(categorical_cols, axis=1)))
-    X_test_final = np.hstack((X_test_categorical.toarray(), X_test.drop(categorical_cols, axis=1)))
+    X_train_final = np.hstack(
+        (X_train_categorical.toarray(), X_train.drop(categorical_cols, axis=1))
+    )
+    X_test_final = np.hstack(
+        (X_test_categorical.toarray(), X_test.drop(categorical_cols, axis=1))
+    )
 
     # Train the model
     model = RandomForestRegressor(n_estimators=100, random_state=42)
@@ -45,18 +51,18 @@ def predict(state, district, season, crop, area):
     Jcrops = crop
     Jarea = area
 
-        
-    #Get the user inputs and store them in a numpy array  - ans is 427.64
-    #user_input = np.array([['Karnataka', 'BAGALKOT', 'Kharif', 'Rice', 197]])
+    # Get the user inputs and store them in a numpy array  - ans is 427.64
+    # user_input = np.array([['Karnataka', 'BAGALKOT', 'Kharif', 'Rice', 197]])
 
-    user_input = np.array([[Jstate,Jdistrict,Jseason,Jcrops,Jarea]])
-
+    user_input = np.array([[Jstate, Jdistrict, Jseason, Jcrops, Jarea]])
 
     # Convert the categorical columns to one-hot encoding
     user_input_categorical = ohe.transform(user_input[:, :4])
 
     # Combine the one-hot encoded categorical columns and numerical columns
-    user_input_final = np.hstack((user_input_categorical.toarray(), user_input[:, 4:].astype(float)))
+    user_input_final = np.hstack(
+        (user_input_categorical.toarray(), user_input[:, 4:].astype(float))
+    )
 
     # Make the prediction
     prediction = model.predict(user_input_final)
@@ -65,6 +71,3 @@ def predict(state, district, season, crop, area):
 
     # print(str(prediction[0]))
     return prediction
-
-
-
